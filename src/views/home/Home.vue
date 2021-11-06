dbef4ba7408af1abca416294a9147248618b9d69
unknown <1218366391@qq.com>
2021-11-07 03:39:15 +0800

    tabbar功能

---
+++ src/views/home/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="home">
+    首页
+  </div>
+</template>
+
+<script>
+ 
+
+  export default {
+    name: "Home",
+    components: {
+      
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      // 1.请求多个数据
+     
+      
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
