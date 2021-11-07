d9b46e6d4343cbc37dc2aa634fcd1c35fb3be2a5
unknown <1218366391@qq.com>
2021-11-07 16:39:45 +0800

    navbar封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="home">
-    首页
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
- 
+  import NavBar from 'components/common/navbar/NavBar'
 
   export default {
     name: "Home",
     components: {
-      
+      NavBar
     },
     data() {
       return {
